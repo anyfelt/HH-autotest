8822ebc35512985595d2621f573d2219b76b9015
anyfelt <sergunchik2005@gmail.com>
2025-08-07 20:48:16 +0300

    Delete CORSproxy.js

--- CORSproxy.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
-app.use(cors());
-app.use('/', createProxyMiddleware({ target: 'http://localhost:1234', changeOrigin: true }));
-app.listen(5555, () => console.log('CORS Proxy на http://localhost:5555'));
